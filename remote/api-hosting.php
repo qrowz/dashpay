703791126a47e39f4683a199fbee953860992558
poiuty <poiuty@lepus.su>
2015-07-17 23:55:18 +0300

    add restart and log

--- remote/api-hosting.php
+++ remote/api-hosting.php
@@ -22,19 +22,28 @@ function conf_dash($command, $ip, $key){
 	}
 }
 
+function dash_restart($ip){
+	shell_exec("dash-cli -datadir=/home/dash/data/$ip stop > /dev/null 2>/dev/null &");
+	sleep(10);
+	shell_exec("dashd -datadir=/home/dash/data/$ip -daemon > /dev/null 2>/dev/null &");
+}
+
 if(empty($_GET['ip']) || empty($_GET['do'])) die('empty');
 if(preg_match('/[^0-9-.]/', $_GET['ip']) || !filter_var($_GET['ip'], FILTER_VALIDATE_IP)) die('wrong_ip');
 $ip = $_GET['ip'];
 
 switch($_GET['do']){
 	default: echo "no"; break;
+	case 'restart': dash_restart($ip); break;
 	case 'setup': 
 		if(empty($_GET['key'])) die('empty_key');
 		if(preg_match('/[^0-9a-zA-z]/', $_GET['key'])) die('wrong_key');
 		conf_dash('remove', $ip, NULL);
 		conf_dash('add', $ip, $_GET['key']);
-		shell_exec("dash-cli -datadir=/home/dash/data/$ip stop > /dev/null 2>/dev/null &");
-		sleep(10);
-		shell_exec("dash-cli -datadir=/home/dash/data/$ip -daemon > /dev/null 2>/dev/null &");
+		dash_restart($ip);
+	break;
+	case 'log':
+		if(!is_dir("/var/www/$ip")) mkdir("/var/www/$ip", 0755);
+		shell_exec("tar -czf /var/www/$ip/debug.tar.gz -C /home/dash/data/$ip/ debug.log");
 	break;
 }
